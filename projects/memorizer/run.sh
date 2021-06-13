526188267bf2601fe83635c78dbbef587cd891e5
Lucas Steven Atayde <lsa4@nightvision.cs.rice.edu>
2021-06-13 15:00:30 -0500

    Added shell script to run the Linuxkit build using qemu

---
+++ projects/memorizer/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# $1: path to vmlinuz
+
+BASEDIR=$(dirname "$BASH_SOURCE")
+NETOPTS="-net nic,model=virtio,macaddr=52:54:00:12:34:56 -net user,hostfwd=tcp:127.0.0.1:5556-:22"
+FILEOPTS="id=fs1,security_model=none -device virtio-9p-pci,fsdev=fs1,mount_tag=host-code"
+GENOPTS="--enable-kvm -m 100G $NETOPTS $FILEOPTS "
+
+qemu-system-x86_64 $GENOPTS \
+    -kernel memorizer-kernel \
+    -initrd ~/rootfs.cpio.gz \
+    -append 'root=/dev/vda1 console=hvc0 nokaslr memalloc_size=10' \
+    -chardev stdio,id=stdio,mux=on,signal=off \
+    -device virtio-serial-pci \
+    -device virtconsole,chardev=stdio \
+    -mon chardev=stdio \
+    -display none \
+    
